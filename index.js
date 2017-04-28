9147f54980fe4face38b6609968d4057223e167d
mukeunkim@gmail.com <thakd100>
2017-04-28 05:24:16 +0000

    .

--- index.js
+++ index.js
@@ -78,3 +78,12 @@ exports.getUniqueId = function() {
     }
     return id;
 };
+
+exports.oformat = function() {
+    var s = arguments[0];
+    for (var i = 0; i < arguments.length - 1; i++) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm");
+        s = s.replace(reg, arguments[i + 1]);
+    }
+    return s;
+};
